05736ba621b2efcbea052e834a3255858bcfd0d5
nskont <nskont@utu.fi>
2024-09-09 20:20:16 +0300

    teht1 valmis

---
+++ teht1.java
@@ -0,0 +1,19 @@
+public class esiintyja {
+    private String tunniste;
+    private String nimi;
+    private String ala;
+    private double palkkio;
+}
+
+public class asiakas {
+    private String nimi;
+    private String email;
+    private String salasana;
+    private int asiakasnumero;
+}
+
+public class tilaisuus {
+    private String tyyppi;
+    private String asiakas;
+    private String esiintyja;
+}
